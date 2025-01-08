ce162ad3080b46f607f8f4fc74550f72325884f5
Steve <steve@kitkat@gmail.com>
2025-01-08 17:56:30 +0700

    Adds cosmetics

--- composeApp/src/commonMain/kotlin/me/androidbox/echojournal/presentation/components/TopicDropDown.kt
+++ composeApp/src/commonMain/kotlin/me/androidbox/echojournal/presentation/components/TopicDropDown.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
@@ -34,12 +33,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -48,7 +44,7 @@ fun TopicDropDown(
     modifier: Modifier = Modifier
 ) {
     var listOfTopics = remember {
-        mutableStateListOf("Jack", "Jared", "Jasper", "Bob", "Peter", "Steve", "Stand", "State")
+        mutableStateListOf("# Jack", "# Jared", "# Jasper", "# Bob", "# Peter", "# Steve", "# Stand", "# State")
     }
 
     var selectedTopics = remember {
@@ -143,9 +139,9 @@ fun TopicDropDown(
                         }
                     )
 
-                    if (listOfTopics.none {
-                            it.contains(searchText, ignoreCase = true)
-                        }) {
+                    if (listOfTopics.firstOrNull {
+                            it.contains(searchText)
+                        } == null) {
                         item {
                             Text(
                                 modifier = Modifier.clickable {
